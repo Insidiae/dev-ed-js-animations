09e1f572fd654efce1bc72ae6a31d749e12a29a9
Kobe Ruado <insidiae423@gmail.com>
2022-04-26 20:51:00 +0800

    Add split text animation

--- text-reveal/app.js
+++ text-reveal/app.js
@@ -44,3 +44,20 @@ timeline.fromTo(
   "<20%"
 );
 timeline.fromTo(".cta__btn", { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, "<");
+
+//? The GSAP SplitText plugin makes this easier, but it's a paid-only feature
+const logo = document.querySelector(".logo");
+const logoLetters = logo.textContent.split("");
+
+logo.innerHTML = "";
+logoLetters.forEach((letter) => {
+  logo.innerHTML += `<span class="letter">${letter}</span>`;
+});
+
+gsap.set(".letter", { display: "inline-block" });
+
+gsap.fromTo(
+  ".letter",
+  { y: "100%" },
+  { y: 0, ease: "back.out(3)", delay: 2, stagger: 0.05 }
+);
